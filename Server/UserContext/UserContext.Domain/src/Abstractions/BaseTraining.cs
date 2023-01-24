246948985327c0650c2a0cc72a73c4484eeab297
icorlli <corlliivan@gmail.com>
2023-01-24 15:06:53 -0300

    Se creo aggregate para entrenamientos

---
+++ Server/UserContext/UserContext.Domain/src/Abstractions/BaseTraining.cs
@@ -0,0 +1,14 @@
+﻿using SharedKernell.src.Entity;
+using UserContext.Domain.src.Interface.Entity;
+
+namespace UserContext.Domain.src.Abstractions;
+
+public abstract class BaseTraining : AggregateRoot, IBaseTraining
+{
+    public string Name { get; protected set; }
+
+    protected BaseTraining(string name) 
+    {
+        Name = name;
+    }
+}
---
+++ Server/UserContext/UserContext.Domain/src/Entity/Training.cs
@@ -0,0 +1,10 @@
+﻿using UserContext.Domain.src.Abstractions;
+
+namespace UserContext.Domain.src.Entity;
+
+public class Training : BaseTraining
+{
+    public Training(string name) : base(name)
+    {
+    }
+}
